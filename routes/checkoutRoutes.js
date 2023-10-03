ef431df074916656b91e2bfedd56db6f4e0981c8
Rammus87 <131853744+Rammus87@users.noreply.github.com>
2023-10-04 01:32:44 +0800

    1003checkout

--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -4,9 +4,20 @@ const path = require('path');
 const connection = require('../config/db');
 
 
-router.get('/checkout',(req,res)=>{
+router.get('/checkout',async(req,res)=>{
     const username = req.session.user ? req.session.user.username : null;
-    res.render('checkout',{ username })
+    try {
+        const user = req.session.user;
+        if (!user) {
+            return res.redirect('/store'); 
+        }
+        const [checkoutItems] = await connection.query(
+            `SELECT shoppingcart.quantity, products.product_name, products.price, products.image FROM shoppingcart JOIN products ON shoppingcart.product_id = products.product_id WHERE shoppingcart.user_id = ?`, [user.id]);
+            res.render('checkout', { username: req.session.user.username, checkoutItems });
+    } catch (error) {
+        console.error('結帳資料錯誤:', error);
+        res.status(500).send('服務器錯誤');
+    }
 })
 
 module.exports = router;
