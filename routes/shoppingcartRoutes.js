b943a76be26ce0565bff9494992b039fa642e27f
Rammus87 <131853744+Rammus87@users.noreply.github.com>
2023-10-04 16:10:33 +0800

    1004checkout

--- routes/shoppingcartRoutes.js
+++ routes/shoppingcartRoutes.js
@@ -17,4 +17,32 @@ router.get('/shoppingcart',async(req,res)=>{
     }
 })
 
+//更新購物車數量
+router.post('/updateQuantity', async (req, res) => {
+    try {
+        const { productId, quantity } = req.body;
+        const user = req.session.user;
+
+        if (!user) {
+            return res.status(403).json({ message: '用户未登录' });
+        }
+
+        // 更新數量
+        const result = await connection.query(
+            'UPDATE shoppingcart SET quantity = ? WHERE user_id = ? AND product_id = ?',
+            [quantity, user.id, productId]
+        );
+
+        if (result.affectedRows === 1) {
+            res.json({ message: '數量已更新' });
+        } else {
+            res.status(404).json({ message: '找不到購物車商品' });
+        }
+    } catch (error) {
+        console.error('更新失敗:', error);
+        res.status(500).json({ message: '服務器' });
+    }
+});
+
+
 module.exports = router;
